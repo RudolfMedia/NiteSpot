a44ea67d08a23b22d348226ba830b50c0e749174
Dan Rudolf <danrudolf@gmail.com>
2015-01-14 19:23:52 -0500

    weird error with drawing of price rect

---
+++ NiteSpot/RMEatDrinkDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  EatDrinkDetailVC.h
+//  NiteSpot
+//
+//  Created by Dan Rudolf on 1/14/15.
+//  Copyright (c) 2015 com.rudolfmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EatDrinkDetailVC : UIViewController
+
+@end
